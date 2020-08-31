d26efb9685d3b5f43b32710bea67d0c7b18fbe37
paco <paco15241@gmail.com>
2020-08-31 21:31:15 +0800

    Part 03 - Checkout with Stripe

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::post('/cart/switchToSaveForLater/{product}', 'CartController@switchToSave
 Route::delete('/saveForLater/{prodduct}', 'SaveForLaterController@destroy')->name('saveForLater.destroy');
 Route::post('/saveForLater/switchToCart/{product}', 'SaveForLaterController@switchToCart')->name('saveForLater.switchToCart');
 
-Route::get('/empty', function() {
-  Cart::instance('saveForLater')->destroy();
-});
+Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
+Route::post('/checkout', 'CheckoutController@store')->name('checkout.store');
 
-Route::view('/checkout', 'checkout');
-Route::view('/thankyou', 'thankyou');
+Route::get('/thankyou', 'ConfirmationController@index')->name('confirmation.index');

04eda5490ff1846abfccfa0161920c7ccae6998a
paco <paco15241@gmail.com>
2020-09-08 11:17:26 +0800

    Part 05 - Categories, Sorting and Pagination

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,7 @@
 function presentPrice($price) {
   return '$'.number_format($price / 100, 2);
 }
+
+function setActiveCategory($category, $output = 'active') {
+  return request()->category == $category ? $output : '';
+}

1dbe1a6cce9e373cb298ab212be7d122879e6fb1
paco <paco15241@gmail.com>
2020-09-20 18:07:50 +0800

    Part 12 - Image Uploads

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,7 @@ function presentPrice($price) {
 function setActiveCategory($category, $output = 'active') {
   return request()->category == $category ? $output : '';
 }
+
+function productImage($path) {
+  return $path && file_exists('storage/' . $path) ? asset('storage/' . $path) : asset('img/not-found.jpg');
+}
